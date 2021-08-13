3a7d934560e4b6a150855e81d026e3399875adb6
Omar Labana <omarlabana@outlook.com>
2021-08-13 22:14:07 +0200

    Add file loader to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -8,4 +8,21 @@ module.exports = {
   },
   mode: 'development',
   watch: true,
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|ogg|mp3|wav|mpe?g)$/,
+        use: [
+          'file-loader',
+        ],
+      },
+    ],
+  },
 };
