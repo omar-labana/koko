29069fea18a35a499348a8623eb8be4518cd9b9a
Omar Labana <omarlabana@outlook.com>
2021-08-13 23:54:12 +0200

    Split modal logic

--- README.md
+++ README.md
@@ -1,2 +1,80 @@
-# koko
-Shooter web game. Built with phaser.js and webpack.
+# Project:  KOKO - Meme Space Shooter Game
+
+### Description
+
+An infinit space shooter game, designed based on a meme. Enemies appear at the top of the canvas Some shoot you, some pass straight, some pursue you. You control you character with keyboard arrows while you shoot with space bar. Just destroy as many enemies as you can before your HP runs out and you die. After killing an enemy some point are added to your total score and you can add your name along with your highest score to the leadersboard.
+
+
+
+### Live Demo
+
+[Live Demo]()
+
+### Instructions
+
+### How to Install
+
+If you want to install the project follow these instructions:
+
+
+
+- Click on the green button "Clone or Download"
+- Click on Download ZIP or clone the repo if you have git installed
+- Extract the game if you dowloaded the zip file
+- In your terminal, navigate to the game's folder
+- Run 'npm install'
+- Run 'npm run start'
+- Open 'localhost:8080/dist' in your browser
+- Start shooting!
+
+### How to play
+
+You only need to press your arrow keys to move you character and use spacebar to shoot the enemies.
+
+Each enemy destroyed will give you some points.
+
+If any of the enemies attacks you, you lose some health. Upon reaching 0 health the game is over and you are presented with a form to submit you name and score(You can skip submition).
+- NOTE: adding you score in the first time might not work because the request might time out when first inviked by herokus' server.
+
+
+## Dependencies
+
+- JavaScript
+- Tailwind CSS
+- Phaser3
+- Webpack
+- Eslint
+- Babel
+- Jest
+
+## Test Suite
+
+- Jest
+
+## NPM Scripts
+
+Use "npm run-script " followed by any of the following commands: 
+
+- "npm run dev" =>  "webpack --mode development",
+- "npm run build" =>  "webpack --mode production",
+- "npm run start" =>  "webpack serve --mode development --open",
+- "npm run test" =>  "jest"
+
+
+## 👤 Author
+
+👤 **Sunday Uche Ezeilo**
+
+- Github: [@omarlabana](https://github.com/omar-labana)
+- Twitter: [@panda4cs](https://twitter.com/panda4cs)
+- Linkedin: [Omar Labana](https://www.linkedin.com/in/omar-labana/)
+
+## 🤝 Contributing
+
+Contributions, issues and feature requests are welcome!
+
+Feel free to check the [issues page](https://github.com/ezeilo-su/space-shooter-game/issues).
+
+## Show your support
+
+Give a ⭐️ if you like this project!
--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,14 +1,133 @@
-const create = (elementName, styles = false, text = false) => {
-  const element = document.createElement(elementName);
-  if (styles) {
-    styles.forEach((style) => {
-      element.classList.add(style);
-    });
-  }
-  if (text) {
-    element.textContent = text;
-  }
-  return element;
-};
 
-export { create }
+function create(type) {
+  return document.createElement(type);
+}
+
+function appendElement(container, element) {
+  container.appendChild(element);
+}
+
+function setDisplay(element) {
+  element.style.display = 'flex';
+  element.style.opacity = '1';
+}
+
+function setTransparency(element) {
+  element.style.opacity = '0';
+  setTimeout(() => { element.style.display = 'none'; }, 600);
+}
+
+function hideView(backView) {
+  setTransparency(backView);
+  setTimeout(() => { backView.remove(); }, 200);
+}
+
+function appAlert(title, text) {
+  const backView = create('div');
+  backView.className = 'backView';
+  appendElement(document.body, backView);
+  backView.setAttribute('tabindex', '0');
+  backView.addEventListener('keypress', e => {
+    if (e.keyCode === 27) {
+      hideView(backView);
+    }
+  });
+
+  const alertPanel = create('div');
+  alertPanel.className = 'alertPanel';
+
+  const alertTitle = create('div');
+  alertTitle.className = 'alertTitle';
+  alertTitle.textContent = title;
+
+  const alertText = create('div');
+  alertText.className = 'alertText';
+  alertText.textContent = text;
+
+  const okAlertButton = create('div');
+  okAlertButton.className = 'okAlertButton';
+  okAlertButton.textContent = 'OK';
+  okAlertButton.addEventListener('click', () => {
+    hideView(backView);
+  });
+
+  appendElement(alertPanel, alertTitle);
+  appendElement(alertPanel, alertText);
+  appendElement(alertPanel, okAlertButton);
+  appendElement(backView, alertPanel);
+  setDisplay(backView);
+}
+
+function appConfirm(callBack, args, title, text) {
+  const backView = create('div');
+  backView.className = 'backView';
+  appendElement(document.body, backView);
+  backView.setAttribute('tabindex', '0');
+  backView.addEventListener('keypress', e => {
+    if (e.keyCode === 27) {
+      hideView(backView);
+    }
+  });
+
+  const alertPanel = create('div');
+  alertPanel.className = 'alertPanel';
+
+  const alertTitle = create('div');
+  alertTitle.className = 'alertTitle';
+  alertTitle.textContent = title;
+
+  const alertText = create('div');
+  alertText.className = 'alertText';
+  alertText.textContent = text;
+
+  const confirmButtons = create('div');
+  confirmButtons.className = 'confirmButtons';
+
+  const yesButton = create('div');
+  yesButton.className = 'okAlertButton';
+  yesButton.textContent = 'Yes';
+  yesButton.addEventListener('click', () => {
+    callBack(args);
+    hideView(backView);
+  });
+
+  const noButton = create('div');
+  noButton.className = 'okAlertButton';
+  noButton.textContent = 'No';
+  noButton.addEventListener('click', () => {
+    hideView(backView);
+  });
+
+  const okAlertButton = create('div');
+  okAlertButton.className = 'okAlertButton';
+  okAlertButton.textContent = 'YES';
+  okAlertButton.addEventListener('click', () => {
+    callBack(args);
+    hideView(backView);
+  });
+
+  appendElement(confirmButtons, yesButton);
+  appendElement(confirmButtons, noButton);
+
+  appendElement(alertPanel, alertTitle);
+  appendElement(alertPanel, alertText);
+  appendElement(alertPanel, confirmButtons);
+  appendElement(backView, alertPanel);
+  setDisplay(backView);
+}
+
+function gat(element, attribute) {
+  return element.getAttribute(attribute);
+}
+
+function sat(element, attribute, value) {
+  element.setAttribute(attribute, value);
+}
+
+export {
+  create,
+  appendElement,
+  appAlert,
+  setDisplay,
+  setTransparency,
+};
